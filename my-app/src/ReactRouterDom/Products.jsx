37d30ab32768eab418c36adf0acecad532681224
manish9427 <manish119427@gmail.com>
2023-02-04 01:12:09 +0530

    4 feb-2

--- my-app/src/ReactRouterDom/Products.jsx
+++ my-app/src/ReactRouterDom/Products.jsx
@@ -1,9 +1,26 @@
 import { useEffect } from "react";
 function Products() {
-  useEffect(() => {}, []);
+  useEffect(() => {
+    fetch("https://fakestoreapi.com/products")
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+      });
+  }, []);
   return (
     <div className="App">
       <h1>Products</h1>
+      <p>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Animi ad
+        laudantium quidem commodi quia iusto nostrum. Distinctio quasi eos
+        dolorum error neque tempore, provident reiciendis placeat ab obcaecati
+        eius voluptatem!
+      </p>
+      {Products.length > 0 ? (
+        <div></div>
+      ) : (
+        <h2 style={{ color: "red" }}>Data is Not Available</h2>
+      )}
     </div>
   );
 }
