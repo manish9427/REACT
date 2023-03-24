ec5601e089e8872853c37228529227bfe0ac5d89
manish9427 <manish119427@gmail.com>
2023-03-25 01:26:19 +0530

    useState

--- my-app/src/ChatGpt/TrackButton.jsx
+++ my-app/src/ChatGpt/TrackButton.jsx
@@ -1,5 +1,11 @@
 import React, { useState } from "react";
 
+const myStyle = {
+  width: "300px",
+  padding: "50px",
+  boxShadow: "0px 0px 10px black",
+  margin: "50px auto",
+};
 export default function MyComponent() {
   const [isClicked, setIsClicked] = useState(false);
 
@@ -8,6 +14,11 @@ export default function MyComponent() {
   };
 
   return (
-    <button onClick={handleClick}>{isClicked ? "Clicked!" : "Click me"}</button>
+    <div style={myStyle}>
+      <h1>Use of useState</h1>
+      <button onClick={handleClick}>
+        {isClicked ? "Clicked!" : "Click me"}
+      </button>
+    </div>
   );
 }
