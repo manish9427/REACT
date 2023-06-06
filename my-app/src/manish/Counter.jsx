0ddd3ee8ab2467fce8365b238b2c5cd62e9a0d33
manish9427 <manish119427@gmail.com>
2023-06-06 19:53:09 +0530

    counter

--- my-app/src/manish/Counter.jsx
+++ my-app/src/manish/Counter.jsx
@@ -1,43 +1,65 @@
-import { useState, useEffect } from "react";
+// import { useState, useEffect } from "react";
 
-const mystyle = {
-  padding: "30px",
-  boxShadow: "0px 0px 10px black",
-  width: "300px",
-  margin: "0px auto",
-  marginTop: "50px",
-};
+// const mystyle = {
+//   padding: "30px",
+//   boxShadow: "0px 0px 10px black",
+//   width: "300px",
+//   margin: "0px auto",
+//   marginTop: "50px",
+// };
 
+// const Counter = () => {
+//   const [state, setState] = useState(0);
+//   useEffect(() => {
+//     const value = localStorage.getItem("count");
+//     if (value) {
+//       setState(parseInt(value));
+//     }
+//   }, []);
+//   useEffect(() => {
+//     localStorage.setItem("count", state);
+//   }, [state]);
+
+//   const add = () => {
+//     setState(state + 1);
+//   };
+//   const sub = () => {
+//     if (state > 0) {
+//       setState(state - 1);
+//     }
+//   };
+//   const reset = () => {
+//     setState(0);
+//   };
+//   return (
+//     <div style={mystyle}>
+//       <h1>count</h1>
+//       <h1>{state}</h1>
+//       <button onClick={add}>ADD</button> &nbsp;
+//       <button onClick={sub}>SUB</button> &nbsp;
+//       <button onClick={reset}>RESET</button>
+//     </div>
+//   );
+// };
+// export default Counter;
+
+import { useState } from "react";
 const Counter = () => {
-  const [state, setState] = useState(0);
-  useEffect(() => {
-    const value = localStorage.getItem("count");
-    if (value) {
-      setState(parseInt(value));
-    }
-  }, []);
-  useEffect(() => {
-    localStorage.setItem("count", state);
-  }, [state]);
+  const [count, setCount] = useState(0);
+  const myStyle = {
+    padding: "30px",
+    width: "300px",
+    boxShadow: "0px 0px 10px black",
+    margin: "0px auto",
+  };
 
-  const add = () => {
-    setState(state + 1);
-  };
-  const sub = () => {
-    if (state > 0) {
-      setState(state - 1);
-    }
-  };
-  const reset = () => {
-    setState(0);
-  };
   return (
-    <div style={mystyle}>
-      <h1>count</h1>
-      <h1>{state}</h1>
-      <button onClick={add}>ADD</button> &nbsp;
-      <button onClick={sub}>SUB</button> &nbsp;
-      <button onClick={reset}>RESET</button>
+    <div style={myStyle}>
+      <h1>Counter</h1>
+      <p>{count}</p>
+      <button onClick={() => setCount(count + 1)}>Add</button>
+      &nbsp;
+      <button onClick={() => setCount(count - 1)}>Sub</button>
     </div>
   );
 };
