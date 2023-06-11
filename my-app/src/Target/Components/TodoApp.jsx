a4ebf3111dc2ee68f2932b9ccefbbeaea3b54c42
manish9427 <manish119427@gmail.com>
2023-06-11 19:54:40 +0530

    new

---
+++ my-app/src/Target/Components/TodoApp.jsx
@@ -0,0 +1,48 @@
+import React, { useState } from "react";
+
+const TodoApp = () => {
+  const [todos, setTodos] = useState([]);
+  const [inputValue, setInputValue] = useState("");
+
+  const addTodo = () => {
+    if (inputValue.trim() !== "") {
+      setTodos([...todos, inputValue]);
+      setInputValue("");
+    }
+  };
+
+  const removeTodo = (index) => {
+    const updatedTodos = [...todos];
+    updatedTodos.splice(index, 1);
+    setTodos(updatedTodos);
+  };
+
+  return (
+    <div>
+      <h1>Todo List</h1>
+      <input
+        type="text"
+        value={inputValue}
+        onChange={(e) => setInputValue(e.target.value)}
+      />
+      <button onClick={addTodo}>Add Todo</button>
+      <ul>
+        {todos.map((todo, index) => (
+          <li key={index}>
+            {todo}
+            <button onClick={() => removeTodo(index)}>Remove</button>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default TodoApp;
+
+/*
+what we are going to see in this 
+1- how to take input from the user 
+2- add that input and show it on to the screen
+3- same input should have a remove button also through which we can remove the inpu
+*/
--- my-app/src/Target/Target.js
+++ my-app/src/Target/Target.js
@@ -2,12 +2,14 @@ import "./Target.css";
 
 import Counter from "./Components/Counter";
 import Timer from "./Components/Timer";
+import TodoApp from "./Components/TodoApp";
 
 function Target() {
   return (
     <div className="Target">
       <Counter />
       <Timer />
+      <TodoApp />
     </div>
   );
 }
