a3261d7d4c3f255d2693b311e648fb5eb5fd6fda
manish9427 <manish119427@gmail.com>
2023-06-17 18:30:13 +0530

    new

--- my-app/src/Target/Components/MyTodo.jsx
+++ my-app/src/Target/Components/MyTodo.jsx
@@ -1,12 +1,25 @@
 import { useState } from "react";
 const MyTodo = () => {
   const [todo, setTodo] = useState([]);
-  const [value, setValue] = useState("");
+  const [inputValue, setInputValue] = useState("");
+
+  const addTodo = () => {
+    if (inputValue.trim() !== "") {
+      setTodo([...todo, inputValue]);
+      setInputValue("");
+    }
+  };
+
   return (
     <div>
       <h1>My Todo</h1>
-      <input type="text" />
-      <button>Add</button>
+      <input
+        type="text"
+        value={inputValue}
+        onChange={(e) => setInputValue(e.target.value)}
+      />
+      <button onClick={addTodo}>Add</button>
+      <ul>{todo.map((todo, index) => {})}</ul>
     </div>
   );
 };
