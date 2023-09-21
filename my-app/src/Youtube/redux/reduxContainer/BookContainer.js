9a409786cb22e2d3dd54e9f6b90452289b0f63df
manish9427 <manish119427@gmail.com>
2023-09-21 09:24:53 +0530

    new

--- my-app/src/Youtube/redux/reduxContainer/BookContainer.js
+++ my-app/src/Youtube/redux/reduxContainer/BookContainer.js
@@ -1,7 +1,24 @@
 import React from "react";
+import { useSelector } from "react-redux";
+
+const style = {
+  boxShadow: "0px 0px 10px black",
+  width: "300px",
+  margin: "10px auto",
+  padding: "30px",
+};
 
 const BookContainer = () => {
-  return <div>BookContainer</div>;
+  const ans = useSelector((state) => state.NumberOfBooks);
+  return (
+    <div style={style}>
+      <h1>useSelector</h1>
+      <p>No. of Books - {ans}</p>
+    </div>
+  );
 };
 
 export default BookContainer;
+
+// state => we can write anything
+// selector function which is returning value
