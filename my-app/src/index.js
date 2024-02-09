64c16a827b258dd7ae01bbdc8b7d3869d947b0a2
Manish <manish119427@gmail.com>
2024-02-09 23:34:27 +0530

    new

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -1,6 +1,8 @@
 import "./index.css";
 import ReactDOM from "react-dom/client";
-import App from "./App";
+// import App from "./App";
+import Manish from "./manish/Manish";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<App />);
+// root.render(<App />);
+root.render(<Manish />);
---
+++ my-app/src/manish/Manish.js
@@ -0,0 +1,11 @@
+import React, { useState } from "react";
+
+const Manish = () => {
+  return (
+    <div className="manish">
+      <h1>Timer</h1>
+    </div>
+  );
+};
+
+export default Manish;
