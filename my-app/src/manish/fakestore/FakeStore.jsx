ccffee72474af46936bcfbe11a79f368e58b831b
manish9427 <manish119427@gmail.com>
2024-05-18 20:24:53 +0530

    new

--- my-app/src/manish/fakestore/FakeStore.jsx
+++ my-app/src/manish/fakestore/FakeStore.jsx
@@ -1,32 +1,31 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faStar } from "@fortawesome/free-regular-svg-icons"; // Import the regular star icon
-
-import { useEffect, useState } from "react";
+import { faStar } from "@fortawesome/free-regular-svg-icons";
 import "../Manish.css";
 
 const FakeStore = () => {
   const [products, setProducts] = useState([]);
+  
   useEffect(() => {
     fetch("https://fakestoreapi.com/products/")
       .then((res) => res.json())
       .then((json) => {
-        console.log(json);
         setProducts(json);
       });
   }, []);
+
   return (
     <div>
       <h1>Store</h1>
       <div className="product-container">
-        {products.map((e) => (
-          <div key={e.id} className="product">
-            <h2>{e.title}</h2>
-            <p>Price: ${e.price}</p>
-            <p>{e.description}</p>
-            <img className="imgFake" src={e.image} alt={e.title} />
-            <div>
-              <span>Rating:{e.rating.rate} </span>
+        {products.map((product) => (
+          <div key={product.id} className="product">
+            <img className="imgFake" src={product.image} alt={product.title} />
+            <h2>{product.title}</h2>
+            <p>Price: ${product.price}</p>
+            <p>{product.description}</p>
+            <div className="rating">
+              <span>Rating: {product.rating.rate}</span>
               <FontAwesomeIcon icon={faStar} />
             </div>
             <button>Add to Basket</button>
