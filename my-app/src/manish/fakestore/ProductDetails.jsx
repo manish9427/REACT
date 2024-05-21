87a13d516f1bab3e518a8a79754609726ded19e4
manish9427 <manish119427@gmail.com>
2024-05-21 23:16:35 +0530

    new

--- my-app/src/manish/fakestore/ProductDetails.jsx
+++ my-app/src/manish/fakestore/ProductDetails.jsx
@@ -1,13 +1,24 @@
 import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faStar } from "@fortawesome/free-regular-svg-icons";
+import "./FakeStore.css"
 import { useParams,useLocation } from "react-router-dom";
 export  const ProductDetails = () => {
     const {state} = useLocation();
     const {product} = state;
     const {id} =useParams();
     return (
-        <div>
-            <h1>{product.title}</h1>
-        </div>
-    )
+    <div >
+        <div className="product-details">
+            <img className="imgDetails" src={product.image} alt={product.title} />
+            <h2>{product.title}</h2>
+            <p>Price: ${product.price}</p>
+            <p>{product.description}</p>
+            <div className="rating">
+              <span>Rating: {product.rating.rate}</span>
+              <FontAwesomeIcon icon={faStar} />
+            </div>
+          </div>
+      </div>);
 
 }
