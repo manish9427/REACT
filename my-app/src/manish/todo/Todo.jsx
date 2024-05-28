4ba647aad0067aa6526db0b9893a56e76f657e29
manish9427 <manish119427@gmail.com>
2024-05-28 23:50:10 +0530

    new

--- my-app/src/manish/todo/Todo.jsx
+++ my-app/src/manish/todo/Todo.jsx
@@ -1,12 +1,30 @@
-const Todo = () => {
+import { useState } from "react";
+export const Todo = () => {
+    const [todo,setTodo]=useState('');
+    const [todos,setTodos]=useState([]);
+    const handleInput = (event) =>{
+        setTodo(event.target.value)
+    }
+
+    const handleSubmit = () => {
+        if(todo.trim()){
+            setTodos([...todos,todo])
+            setTodo('');
+        }
+
+    }
+
     return (
         <div>
-            <h1>
-                Todo
-            </h1>
+            <h1>todo</h1>
+            <input type="text" onChange={handleInput}  value={todo}/>
+            <button onClick={handleSubmit}>Submit</button>
 
+            <ul>{todos.map((value,index)=>(
+                <li key={index}>
+                    {value}
+                </li>
+            ))}</ul>
         </div>
     )    
 }
-
-export default Todo;
