c8ec8914014b81f59b653035fdd03ef38257a9db
manish9427 <manish119427@gmail.com>
2024-06-12 23:49:39 +0530

    new

--- my-app/src/manish/homePage/HomePage.jsx
+++ my-app/src/manish/homePage/HomePage.jsx
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom';
 const HomePage = () => {
   return (
     <div>
-      <h1>Home Page</h1>
+      <h1>Product Page</h1>
       <ul>
         <li><Link to="/counter">Counter</Link></li>
         <li><Link to="/timer">Timer</Link></li>
