33b189dfb69d3e8c2314ec553c4096900fffc88b
manish9427 <manish119427@gmail.com>
2024-06-13 23:27:40 +0530

    new

--- my-app/src/manish/homePage/HomePage.jsx
+++ my-app/src/manish/homePage/HomePage.jsx
@@ -1,16 +1,17 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import "./HomePage.css"
 
 const HomePage = () => {
   return (
-    <div>
-      <h1>Product Page</h1>
-      <ul>
-        <li><Link to="/counter">Counter</Link></li>
-        <li><Link to="/timer">Timer</Link></li>
-        <li><Link to="/stopwatch">StopWatch</Link></li>
-        <li><Link to="/fakestore">FakeStore</Link></li>
-        <li><Link to="/todo">Todo</Link></li>
+    <div className='home-container'>
+      <h1 className='home-title'>Product Page</h1>
+      <ul className='home-list'>
+        <li><Link className='home-link' to="/counter">Counter</Link></li>
+        <li><Link className='home-link' to="/timer">Timer</Link></li>
+        <li><Link className='home-link' to="/stopwatch">StopWatch</Link></li>
+        <li><Link className='home-link' to="/fakestore">FakeStore</Link></li>
+        <li><Link className='home-link' to="/todo">Todo</Link></li>
       </ul>
     </div>
   );
