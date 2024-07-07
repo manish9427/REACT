f1f8d1109af6be7983e2456317c1da14079ce320
manish9427 <manish119427@gmail.com>
2024-07-07 23:14:16 +0530

    new

--- my-app/src/manish/fakestore/FakeStore.jsx
+++ my-app/src/manish/fakestore/FakeStore.jsx
@@ -21,7 +21,7 @@ const FakeStore = () => {
 
   return (
     <div>
-      <h1>Fake Store</h1>
+      <h1>Fake Store API</h1>
       <div className="product-container">
         {products.map((product) => (
           <div key={product.id} className="product">
