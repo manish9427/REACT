381c9a8d54598580b86824431beccb90addfc5f3
manish9427 <manish119427@gmail.com>
2024-07-09 23:52:01 +0530

    new

--- my-app/src/manish/stopwatch/StopWatch.jsx
+++ my-app/src/manish/stopwatch/StopWatch.jsx
@@ -12,7 +12,7 @@ const StopWatch = () => {
     let interval;
     if (isRunning && !isPaused) {
       interval = setInterval(() => {
-        setTime((prevTime) => prevTime + 10); // Update time every 10 milliseconds
+        setTime((prevTime) => prevTime + 10); // Update time every 10 ms
       }, 10); // Update every 10 milliseconds
     } else {
       clearInterval(interval);
