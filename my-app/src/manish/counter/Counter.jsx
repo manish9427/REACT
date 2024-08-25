0975ee4257aaa54fa225fe11d7f522de0d10a9bb
manish9427 <manish119427@gmail.com>
2024-08-25 23:18:01 +0530

    new

--- my-app/src/manish/counter/Counter.jsx
+++ my-app/src/manish/counter/Counter.jsx
@@ -1,8 +1,27 @@
-import React from 'react'
+import React,{useState} from 'react'
 
 const Counter = () => {
+  const[count,setCount]=useState(0);
+  const add = () => {
+    setCount(count + 1);
+  }
+  const sub =() => {
+    if(count>0){
+
+      setCount(count - 1);
+    }
+  }
+  const reset =() => {
+    setCount(0);
+  }
   return (
-    <div>Counter</div>
+    <div>
+      <h1>Counter</h1>
+      <p>Count: {count}</p>
+      <button onClick={add}>Add</button>
+      <button onClick={sub}>Sub</button>
+      <button onClick={reset}>Reset</button>
+      </div>
   )
 }
 
